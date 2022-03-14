000a7661c1e0a80403ebff058f546ad9b1f0b882
Martin Liu <liumartin@google.com>
2022-03-14 22:45:20 +0800

    rodin: Disable memcg
    Pixel kernel don't use MEMCG, let's disable it to
    minimize the overhead.
    
    Bug: 224521042
    Test: check cmdline
    Signed-off-by: Martin Liu <liumartin@google.com>
    Change-Id: I60681fb04e4f9a41814eda5988b6c1a0d4fb127c
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -156,6 +156,7 @@ BOARD_KERNEL_CMDLINE += androidboot.serialconsole=0
 BOARD_KERNEL_CMDLINE += log_buf_len=1024K
 BOARD_KERNEL_CMDLINE += sysctl.kernel.sched_pelt_multiplier=4
 BOARD_KERNEL_CMDLINE += cgroup.memory=nokmem
+BOARD_KERNEL_CMDLINE += cgroup_disable=memory
 
 BOARD_BOOT_HEADER_VERSION := 4
 
