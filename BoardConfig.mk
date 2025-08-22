dbe0f015eeb880c51f96b76650456eca1d9108e4
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-22 16:11:42 +0800

    rodin: Use the proper way to disable console service
    Change-Id: Ie5d1c7c9b4937eaeb0c279b2427ddae23bc9f4a7
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -152,6 +152,7 @@ AB_OTA_PARTITIONS += \
 
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_CMDLINE += rcu_nocbs=all rcutree.enable_rcu_lazy=1
+BOARD_KERNEL_CMDLINE += androidboot.serialconsole=0
 
 BOARD_BOOT_HEADER_VERSION := 4
 
