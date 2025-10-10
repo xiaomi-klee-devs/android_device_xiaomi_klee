9dfd11bc3a86ee875de263d63a24c1595e80d177
Ido <dev.xyzuniverse@gmail.com>
2025-10-10 10:07:47 +0200

    rodin: Upstream blobs into OS2.0.204.0.VOJMIXM
    Change-Id: Ia872f8a29767c0828ec1d6d1286a7d422bac9a9d
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- lineage_rodin.mk
+++ lineage_rodin.mk
@@ -26,8 +26,8 @@ PRODUCT_SYSTEM_DEVICE := rodin
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="missi-user 15 AP3A.240905.015.A2 OS2.0.101.0.VOJMIXM release-keys" \
-    BuildFingerprint=POCO/rodin_global/rodin:15/AP3A.240905.015.A2/OS2.0.101.0.VOJMIXM:user/release-keys \
+    BuildDesc="missi-user 15 AP3A.240905.015.A2 OS2.0.204.0.VOJMIXM release-keys" \
+    BuildFingerprint=POCO/rodin_global/rodin:15/AP3A.240905.015.A2/OS2.0.204.0.VOJMIXM:user/release-keys \
     DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
 
