f35f91df1e862b747ae9e65f9f192fd779cb53a7
Mashopy <eliasgheeraert@gmail.com>
2025-11-08 18:19:55 +0100

    rodin: Build mediaTek WiFi lib and enable OUI support on wpa_supplicant_8
    Change-Id: Ica8ca6d5eed10b1a36a32ecf552a700ba5433c51
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -372,6 +372,8 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # WiFi
+$(call soong_config_set,wpa_supplicant_8,board_wlan_mediatek_stability,true)
+
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     hostapd \
