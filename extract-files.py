e3d5163b8dd6556bdb43470cf8a8e67ecd61408e
Ido <dev.xyzuniverse@gmail.com>
2026-04-14 14:54:50 +0200

    rodin: Add `hardware/mediatek/libaedv` to namespace
    Change-Id: Ic6e8867b497771bba75708e945ba581f6364a875
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -33,6 +33,7 @@ from extract_utils.utils import (
 namespace_imports = [
     'device/xiaomi/rodin',
     'hardware/mediatek',
+    'hardware/mediatek/libaedv',
     'hardware/mediatek/libmtkperf_client',
     'hardware/xiaomi',
 ]
