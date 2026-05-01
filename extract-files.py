Skizoniee/android_device_xiaomi_rodin

rodin: Link the v34 variant of libui for a few camera blobs
* Required after Android 16.

Change-Id: I4c801ad97d5a518e9fc7087ef1bcc2d33ba623a3
Signed-off-by: claxten10 <claxten10@gmail.com>
Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

extract-files.py
@@ -78,8 +78,6 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/libcodec2_vpp_AIMEMC_plugin.so',
         'vendor/lib64/libcodec2_vpp_AISR_plugin.so',
         'vendor/lib64/libgpud.so',
-        'vendor/lib64/mt6899/libmtkcam_grallocutils.so',
-        'vendor/lib64/libmtkcam_grallocutils_aidlv2helper.so',
         'vendor/lib64/egl/mt6899/libGLES_mali.so',
         'vendor/lib64/hw/mt6899/android.hardware.graphics.allocator-V2-mediatek.so',
         'vendor/lib64/hw/mt6899/mapper.mediatek.so',
@@ -103,11 +101,9 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/vendor.xiaomi.hardware.camera.injection-service.so'
     ): blob_fixup()
         .replace_needed('android.hardware.camera.device-V1-ndk.so', 'android.hardware.camera.device-V2-ndk.so'),
-    ('vendor/lib64/libmicamera_hal_core.so',
-     'vendor/lib64/libcameraopt.so',
+    ('vendor/lib64/libcameraopt.so',
      'vendor/lib64/mt6899/libcam.hal3a.so',
      'vendor/lib64/mt6899/libcam.hal3a.ctrl.so',
-     'vendor/lib64/libmialgoengine.so',
      'vendor/lib64/mt6899/libmtkcam_taskmgr.so',
      'vendor/lib64/hw/hwcomposer.mtk_common.so'): blob_fixup()
         .add_needed('libprocessgroup_shim.so'),
@@ -146,6 +142,15 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libutils-v32.so'),
     'vendor/etc/init/vendor.xiaomi.hardware.vibratorfeature.service.rc': blob_fixup()
         .regex_replace('odm', 'vendor'),
+    'vendor/lib64/libmicamera_hal_core.so': blob_fixup()
+        .add_needed('libprocessgroup_shim.so')
+        .replace_needed('libui.so', 'libui-v34.so'),
+    (
+        'vendor/lib64/mt6899/libmtkcam_grallocutils.so',
+        'vendor/lib64/libmtkcam_grallocutils_aidlv2helper.so',
+    ): blob_fixup()
+        .replace_needed('libui.so', 'libui-v34.so')
+        .replace_needed('android.hardware.graphics.common-V5-ndk.so', 'android.hardware.graphics.common-V6-ndk.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(

xiaomi-klee-devs/android_device_xiaomi_klee

extract-files.py
@@ -78,8 +78,6 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/libcodec2_vpp_AIMEMC_plugin.so',
         'vendor/lib64/libcodec2_vpp_AISR_plugin.so',
         'vendor/lib64/libgpud.so',
-        'vendor/lib64/mt6899/libmtkcam_grallocutils.so',
-        'vendor/lib64/libmtkcam_grallocutils_aidlv2helper.so',
         'vendor/lib64/egl/mt6899/libGLES_mali.so',
         'vendor/lib64/hw/mt6899/android.hardware.graphics.allocator-V2-mediatek.so',
         'vendor/lib64/hw/mt6899/mapper.mediatek.so',
@@ -103,11 +101,9 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/vendor.xiaomi.hardware.camera.injection-service.so'
     ): blob_fixup()
         .replace_needed('android.hardware.camera.device-V1-ndk.so', 'android.hardware.camera.device-V2-ndk.so'),
-    ('vendor/lib64/libmicamera_hal_core.so',
-     'vendor/lib64/libcameraopt.so',
+    ('vendor/lib64/libcameraopt.so',
      'vendor/lib64/mt6899/libcam.hal3a.so',
      'vendor/lib64/mt6899/libcam.hal3a.ctrl.so',
-     'vendor/lib64/libmialgoengine.so',
      'vendor/lib64/mt6899/libmtkcam_taskmgr.so',
      'vendor/lib64/hw/hwcomposer.mtk_common.so'): blob_fixup()
         .add_needed('libprocessgroup_shim.so'),
@@ -144,6 +140,15 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed("libstagefright_foundation.so", "libstagefright_foundation-v33.so"),
     'vendor/bin/hw/mtkfusionrild': blob_fixup()
         .add_needed('libutils-v32.so'),
+    'vendor/lib64/libmicamera_hal_core.so': blob_fixup()
+        .add_needed('libprocessgroup_shim.so')
+        .replace_needed('libui.so', 'libui-v34.so'),
+    (
+        'vendor/lib64/mt6899/libmtkcam_grallocutils.so',
+        'vendor/lib64/libmtkcam_grallocutils_aidlv2helper.so',
+    ): blob_fixup()
+        .replace_needed('libui.so', 'libui-v34.so')
+        .replace_needed('android.hardware.graphics.common-V5-ndk.so', 'android.hardware.graphics.common-V7-ndk.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(