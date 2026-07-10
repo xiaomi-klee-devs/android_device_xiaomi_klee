d2c2b89e6fa2cb89ae7d00e0ecf6a59c96b72562
Mnskkyy <mansur180705@gmail.com>
2026-07-10 02:22:31 +0000

    Revert "rodin: Import HotwordEnrollment blobs"
    This reverts commit aa0db53643fffa9e9def53285940f019a456a133.
    
    Change-Id: I9fdac9408195574ff7a7af1510181b1f70b48211
    Signed-off-by: SkyyVerse <mnskkyy@gmail.com>
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>

--- device.mk
+++ device.mk
@@ -260,10 +260,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.example \
     android.hardware.health-service.example_recovery
 
-# HotwordEnrollement
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
-
 # Keymint
 PRODUCT_PACKAGES += \
     android.hardware.hardware_keystore_V3.xml
