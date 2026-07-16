bca9a4962f2a0e688afa46c18c80d42482a02137
kylieeXD <khayloaftck@gmail.com>
2026-07-16 19:54:44 +0700

    overlay: Set default Settings.Global.DEVICE_NAME according to MIUI
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>

--- device.mk
+++ device.mk
@@ -312,6 +312,7 @@ PRODUCT_PACKAGES += \
     FrameworksResOverlayKlee \
     NfcOverlayKlee \
     SettingsResOverlayKlee \
+    SettingsProviderOverlayKlee \
     SystemUIOverlayKlee \
     TetheringConfigOverlay
 
