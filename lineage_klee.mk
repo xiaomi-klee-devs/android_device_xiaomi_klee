ddde4e03fd9be5a8970be26d976a2364941a2a2f
kylieeXD <khayloaftck@gmail.com>
2026-07-18 13:26:36 +0000

    [TMP]: klee: @LineageOS Bringup
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>

---
+++ lineage_klee.mk
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from klee device
+$(call inherit-product, device/xiaomi/klee/device.mk)
+
+# Lineage build flags
+PRODUCT_CHECK_PREBUILT_MAX_PAGE_SIZE := false
+
+# Device manufacturer
+PRODUCT_DEVICE := klee
+PRODUCT_NAME := lineage_klee
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := 2511FPC34G
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_BRAND_FOR_ATTESTATION := $(PRODUCT_BRAND)
+PRODUCT_DEVICE_FOR_ATTESTATION := $(PRODUCT_DEVICE)
+PRODUCT_MODEL_FOR_ATTESTATION := $(PRODUCT_MODEL)
+PRODUCT_NAME_FOR_ATTESTATION := klee_global
+PRODUCT_MANUFACTURER_FOR_ATTESTATION := $(PRODUCT_MANUFACTURER)
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="missi-user 16 BP2A.250605.031.A3 16OS3.1.260610.194508675.MTPEGL.S release-keys" \
+    BuildFingerprint=POCO/klee_global/klee:16/BP2A.250605.031.A3/OS3.0.304.0.WPJMIXM:user/release-keys \
+    DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
+    DeviceProduct=$(PRODUCT_SYSTEM_NAME) \
+    SystemDevice=$(PRODUCT_SYSTEM_DEVICE) \
+    SystemName=$(PRODUCT_SYSTEM_NAME)
